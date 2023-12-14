2dcd2ffcb62c15d6c1cae298d5f377b6a3d9319d
Margarita Golubeva <margaret.golubeva@gmail.com>
2023-12-14 22:13:18 +0300

    feat: initiate a binary search tree, return root, add a node

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,49 +1,77 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require("../extensions/list-tree.js");
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 class BinarySearchTree {
-
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this._root = null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  root() {
+    return this._root;
+  }
+
+  // helper method which creates a new node to
+  // be inserted and calls insertNode
+  add(data) {
+    // Creating a node and initialising with data
+    let newNode = new Node(data);
+
+    // root is null then node will be added to the tree and made root.
+    if (this._root === null) this._root = newNode;
+    // find the correct position in the tree and add the node
+    else this.insertNode(this._root, newNode);
+  }
+
+  // Method to insert a node in a tree. It moves over the tree to find the location to insert a node with a given data
+  insertNode(node, newNode) {
+    // if the data is less than the node data move left of the tree
+    if (newNode.data < node.data) {
+      // if left is null insert node here
+      if (node.left === null) node.left = newNode;
+      // if left is not null recur until null is found
+      else this.insertNode(node.left, newNode);
+    }
+
+    // if the data is more than the node data move right of the tree
+    else {
+      // if right is null insert node here
+      if (node.right === null) node.right = newNode;
+      // if right is not null recur until null is found
+      else this.insertNode(node.right, newNode);
+    }
   }
 
   has(/* data */) {
-    throw new NotImplementedError('Not implemented');
+    throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
   }
 
   find(/* data */) {
-    throw new NotImplementedError('Not implemented');
+    throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
   }
 
   remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
+    throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
+    throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
+    throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
   }
 }
 
 module.exports = {
-  BinarySearchTree
+  BinarySearchTree,
 };
